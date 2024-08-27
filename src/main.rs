1cd2295dc4a6910457a326316cd9a182127263f7
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2024-08-26 19:09:10 -0600

    Starting to learn chumsky

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
+#![allow(unused)]
+
 use std::io;
 use std::io::Write;
 
+use lparser::BinExpr;
+mod chumsky;
+
 fn main() {
-    let repl_prompt: String = String::from(">> ");
-
-    loop {
-        print!("{repl_prompt}");
-        io::stdout().flush().unwrap();
-
-        let mut buffer = String::new();
-        io::stdin().read_line(&mut buffer).unwrap();
-        print!("{buffer}");
-    }
 }
 
 // Lexer and parser for our lisp repl.
 mod lparser {
     use core::panic;
 
-    enum Operation {
+    #[derive(Debug)]
+    pub enum Operation {
         ADD,
         SUB,
         MUL,
@@ -26,25 +22,28 @@ mod lparser {
         NEG,
     }
 
-    enum Expr {
+    #[derive(Debug)]
+    pub enum Expr {
         BinExpr(BinExpr),
         UnaryExpr(UnaryExpr),
         Number(i32),
     }
 
-    struct BinExpr {
+    #[derive(Debug)]
+    pub struct BinExpr {
         op: Operation,
         left: Box<Expr>,
         right: Box<Expr>,
     }
 
-    struct UnaryExpr {
+    #[derive(Debug)]
+    pub struct UnaryExpr {
         op: Operation,
         operand: Box<Expr>,
     }
 
     impl BinExpr {
-        fn new(op: Operation, left: Box<Expr>, right: Box<Expr>) -> BinExpr {
+        pub(crate) fn new(op: Operation, left: Box<Expr>, right: Box<Expr>) -> BinExpr {
             BinExpr {
                 op: op,
                 left: left,
@@ -52,7 +51,7 @@ mod lparser {
             }
         }
 
-        pub fn eval(&self) -> i32 {
+        fn eval(&self) -> i32 {
             let left = self.left.eval();
             let right = self.right.eval();
 
@@ -67,7 +66,7 @@ mod lparser {
     }
 
     impl UnaryExpr {
-        fn new(op: Operation, operand: Box<Expr>) -> UnaryExpr {
+        pub(crate) fn new(op: Operation, operand: Box<Expr>) -> UnaryExpr {
             UnaryExpr {
                 op: op,
                 operand: operand,
@@ -83,12 +82,71 @@ mod lparser {
     }
 
     impl Expr {
-        fn eval(&self) -> i32 {
+        pub fn eval(&self) -> i32 {
             match self {
                 Expr::BinExpr(be) => be.eval(),
                 Expr::UnaryExpr(ue) => ue.eval(),
                 Expr::Number(n) => *n,
             }
         }
+    }
+}
+
+mod tests {
+    use crate::lparser::{self, BinExpr, UnaryExpr};
+
+    #[test]
+    fn test_eval() {
+        use lparser::Expr;
+        use lparser::Operation::*;
+
+        let expr = Expr::BinExpr(BinExpr::new(
+            ADD,
+            Box::new(Expr::Number(1)),
+            Box::new(Expr::Number(2)),
+        ));
+        assert_eq!(3, expr.eval());
+
+        let expr = Expr::BinExpr(BinExpr::new(
+            SUB,
+            Box::new(Expr::Number(1)),
+            Box::new(Expr::Number(2)),
+        ));
+        assert_eq!(-1, expr.eval());
+
+        let expr = Expr::BinExpr(BinExpr::new(
+            MUL,
+            Box::new(Expr::Number(1)),
+            Box::new(Expr::Number(2)),
+        ));
+        assert_eq!(2, expr.eval());
+
+        let expr = Expr::BinExpr(BinExpr::new(
+            DIV,
+            Box::new(Expr::Number(2)),
+            Box::new(Expr::Number(4)),
+        ));
+        assert_eq!(0, expr.eval());
+
+        let expr = Expr::UnaryExpr(UnaryExpr::new(
+            NEG,
+            Box::new(Expr::Number(4)),
+        ));
+        assert_eq!(-4, expr.eval());
+        let expr = Expr::UnaryExpr(UnaryExpr::new(
+            ADD,
+            Box::new(Expr::Number(4)),
+        ));
+
+        // Combine two expressions, and check.
+        let expr = Expr::BinExpr(BinExpr::new(
+            ADD,
+            Box::new(Expr::Number(1)),
+            Box::new(Expr::UnaryExpr(UnaryExpr::new(
+                NEG,
+                Box::new(Expr::Number(2)),
+            ))),
+        ));
+        assert_eq!(-1, expr.eval());
     }
 }
