2cbae58a49ab5128196b6ce8ccccc4998d37e4d0
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2024-08-28 19:41:28 -0600

    Removed chumsky parser

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,4 @@
 use std::io;
 use std::io::Write;
 
-mod chumsky;
-
 fn main() {}
-
-// Lexer and parser for our lisp repl.
-mod lparser {
-    use core::panic;
-
-    #[derive(Debug)]
-    pub enum Expr {
-        // Binary expression
-        AddExpr(Box<Expr>, Box<Expr>),
-        SubExpr(Box<Expr>, Box<Expr>),
-        MulExpr(Box<Expr>, Box<Expr>),
-        DivExpr(Box<Expr>, Box<Expr>),
-
-        // Unary expression
-        NegExpr(Box<Expr>),
-
-        // Number
-        Number(i32),
-    }
-
-    impl Expr {
-        pub fn eval(&self) -> i32 {
-            match self {
-                Expr::AddExpr(left, right) => left.eval() + right.eval(),
-                Expr::SubExpr(left, right) => left.eval() - right.eval(),
-                Expr::MulExpr(left, right) => left.eval() * right.eval(),
-                Expr::DivExpr(left, right) => left.eval() / right.eval(),
-                Expr::NegExpr(expr) => -expr.eval(),
-                Expr::Number(n) => *n,
-            }
-        }
-    }
-}
